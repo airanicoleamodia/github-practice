46862262b5b9f74236ec3700e025e64404ec978e
maluyacode <adlawandavemerc98@gmail.com>
2023-11-02 22:35:38 +0800

    secret

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Added js")
+PORT=4000
+
+DB_URI=mongodb+srv://adlawandavemerc:adlawan16@cluster0.czcvvvr.mongodb.net/movie-ticketing-system?retryWrites=true&w=majority
+
+JWT_SECRET=9VF8QYwnL9dROUb7Gywm
+JWT_EXPIRES_TIME=7d
+COOKIE_EXPIRES_TIME=7
+
+CLOUDINARY_NAME=dzyv6cl5r
+CLOUDINARY_API_KEY=154527411921563
+CLOUDINARY_API_SECRET=l63sZnWCjiAJah8b7rhxmUCjcOE
